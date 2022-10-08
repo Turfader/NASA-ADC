b83def2fa057091f54bba3d88695145c06355a87
Turfader <109704002+Turfader@users.noreply.github.com>
2022-10-07 17:35:35 -0700

    Update Cartographer.py

--- Cartographer.py
+++ Cartographer.py
@@ -15,24 +15,11 @@ with open("C:/Users/Owner/Documents/NASA project/Raw Data/Rectangular Coordinate
     csv_reader = csv.reader(csv_file, delimiter=',')
     full_list = list(csv_reader)
 
-for i in range(1, len(full_list)):
-    for j in range(4):
-        if j == 0:
-            x_coords.append(float(full_list[i][j]))
-        elif j == 1:
-            y_coords.append(float(full_list[i][j]))
-        elif j == 2:
-            heights.append(float(full_list[i][j]))
-        else:
-            slopes.append(float(full_list[i][j]))
-
 
 def calculate_color(height):
     color = ((height+2872)*255/4830)
     return color, color, color
 
-
-print(calculate_color(heights[0]))
 
 # min x = -31 062
 # max x = 20 047
@@ -42,25 +29,26 @@ print(calculate_color(heights[0]))
 
 
 def draw_points():
-    for i in range(len(slopes)):
-        x_pos = (x_coords[i]/100) + 350
-        y_pos = (y_coords[i]/100) + 250
-        color = calculate_color(heights[i])
-        gfxdraw.pixel(screen, int(x_pos), int(y_pos), color) # note that there is a bit of data loss here.
+    for i in range(1, len(full_list)):
+        x_pos = (float(full_list[i][0])/100) + 350
+        y_pos = (float(full_list[i][1])/100) + 250
+        color = calculate_color(float(full_list[i][2]))
+        gfxdraw.pixel(screen, int(x_pos), int(y_pos), color)  # note that there is a bit of data loss here.
         # Ideally, we'd make the final image have a size equal to the maximum span of the x and y data
 
 
 def draw_slopes():
-    for i in range(len(slopes)):
-        x_pos = (x_coords[i]/100) + 350
-        y_pos = (y_coords[i]/100) + 250
-        if slopes[i] >= 20:
+    for i in range(1, len(full_list)):
+        x_pos = (float(full_list[i][0])/100) + 350
+        y_pos = (float(full_list[i][1])/100) + 250
+        if float(full_list[i][3]) >= 20:
             color = (255, 0, 0)
-        elif slopes[i] >= 8:
+        elif float(full_list[i][3]) >= 8:
             color = (255, 165, 0)
         else:
             color = (50, 150, 50)
         gfxdraw.pixel(screen, int(x_pos), int(y_pos), color)
+
 
 print("initiating pygame")
 pygame.init()
@@ -76,8 +64,8 @@ while not done:
 
     screen.fill((0, 0, 255))
 
-    # draw_points()
+    draw_points()
     # draw_slopes()
-    
+
     pygame.display.flip()
     clock.tick(60)
