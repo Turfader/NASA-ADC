aa07dc7c61cd1fe7a29ca49d2a1801b5a0408eac
Turfader <109704002+Turfader@users.noreply.github.com>
2022-11-01 14:58:03 -0700

    Update BlenderVerts.py

--- BlenderVerts.py
+++ BlenderVerts.py
@@ -9,15 +9,27 @@ verts = []
 edges = []
 faces = []
 
-csvpoints= "C:\\test.csv"     #path name for the csv output by DataProcesser.py 
+
+def makePolys(input):
+    if input%1276 == 0:
+        return
+    faces.append([ input, (input+1277+1), input+1 ]) # change this later
+    faces.append([ input, input+1277, (input+1277+1) ])
+
+csvpoints= "C:/Users/Owner/PycharmProjects/NASA_Artemis_ADC/Artemis_ADC/Raw Data/Rectangular Coordinate Data.csv"     #path name for the csv output by DataProcesser.py 
 pointsReader = csv.reader(open(csvpoints, newline=''), delimiter=',')   
 
 with open(csvpoints, 'rt', encoding="utf8") as csvfile:
     pointsReader = csv.reader(csvfile, delimiter=',')
     for idx, row in enumerate(pointsReader):
         if (idx > 0):
-            vert = (float(row[0]), float(row[1]), float(row[2])) 
+            vert = (float(row[0]), float(row[1]), float(row[2])+3000) 
             verts.append(vert)
+            
+
+for i in range(len(verts)-1277):
+    makePolys(i)
+
 
 obj = bpy.context.object
 
@@ -26,7 +38,7 @@ mesh = bpy.data.meshes.new("wave")
 object = bpy.data.objects.new("wave",mesh)
 
 #create mesh from python data
-mesh.from_pydata(verts,[],[])
+mesh.from_pydata(verts,[],faces) # doesn't work, but almost does
 mesh.update(calc_edges=True)
 
 #set mesh location
