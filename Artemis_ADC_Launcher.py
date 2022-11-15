bba05b16b17c575674066e2b8fc48d4a862d421d
abhi arya <abhia9167@gmail.com>
2022-11-15 11:30:08 -0800

    Added Display.py

--- Artemis_ADC_Launcher.py
+++ Artemis_ADC_Launcher.py
@@ -10,3 +10,7 @@ data_processor_program = run(["cmd", "/c", data_processor_path])
 # runs Cartographer.py
 cartographer_path = fc.path_sub2.replace("\\", "/") + "/Cartographer.py"
 cartographer_program = run(["cmd", "/c", cartographer_path])
+
+# runs Display.py
+display_path = fc.path_sub2.replace("\\", "/") + "/Display.py"
+display_program = run(["cmd", "/c", display_path])
