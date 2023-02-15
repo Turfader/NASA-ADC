d0612c9cd8ba77de7bf4bc85d02103cee812e172
Turfader <109704002+Turfader@users.noreply.github.com>
2023-02-15 11:17:38 -0800

    Update DataProcessor.py

--- DataProcessor.py
+++ DataProcessor.py
@@ -61,63 +61,69 @@ def generate_data_array():
 
 
 # Helper Functions for Math
-def convert_degrees_to_meters(deg):  # takes in degrees latitude
-    return (30366 + 1 / 9) * (90 + float(deg))
-def x_cord_from_polar(r, theta):
-    return r * math.cos(float(theta) * math.pi / 180)
-def y_cord_from_polar(r, theta):
-    return r * math.sin(float(theta) * math.pi / 180)
+def get_x_coord(lat, long, rad):  # takes in degrees latitude and longitude
+    return rad * cos(lat) * cos(long)
+
+
+def get_y_coord(lat, long, rad):
+    return rad * cos(lat) * sin(long)
+
+
+def get_z_coord(lat, long, rad):  # long is technically not used here. I kept it for consistency. -JL
+    return rad * sin(lat)
 
 
 def write_rect_file(data_arr):
     rect_coord_path = fc.path_sub1.replace("\\", "/") + "/ProcessedCoordinateData.csv"  # Processed Data Folder given from FolderCreator.py
-    # min_x = 0
-    # min_y = 0
+
     with open(rect_coord_path, mode="w", newline="") as datafile:
         csv_writer = csv.writer(datafile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(['x cord (in meters)', 'y cord (in meters)', 'height (in meters)', 'slope (in degrees)'])
         for i in range(len(data_arr)):
-            r_meters = convert_degrees_to_meters(data_arr[i][0])  # 0 is Latitude
-            x = x_cord_from_polar(r_meters, data_arr[i][1])  # 1 is Longitude
-            y = y_cord_from_polar(r_meters, data_arr[i][1])
-            csv_writer.writerow([x, y, data_arr[i][2],  # 2 is Height
-                                 data_arr[i][3]])  # 3 is Slope
+            lunar_rad = (1737.4 * 1000)  # converts provided lunar rad data to meters
+            lat = data_arr[i][0]
+            long = data_arr[i][1]
+            height = data_arr[i][2]
+            slope = data_arr[i][3]
+            radius = lunar_rad + height
+            
+            x = get_x_coord(lat, long, radius)
+            y = get_y_coord(lat, long, radius)
+            z = get_z_coord(lat, long, radius)
+            
+            csv_writer.writerow([x, y, z, slope])
         datafile.close()
 
     return rect_coord_path
-            # if x < min_x:
-            #    min_x = x
-            # if y < min_y:
-            #    min_y = y
-    # print("min x: ", min_x)
-    # print("min y: ", min_y)
+
 
 def write_misc_file(min_height):
     misc_path = fc.path_sub1.replace("\\", "/") + "/MiscData.csv"
 
     with open(misc_path, mode="w", newline="") as f:
         csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([x_and_y_dim]) # Line 1
-        csv_writer.writerow([DISTANCE_BETWEEN_POINTS]) # Line 2
-        csv_writer.writerow([min_height]) # Line 3
+        csv_writer.writerow([x_and_y_dim])  # Line 1
+        csv_writer.writerow([DISTANCE_BETWEEN_POINTS])  # Line 2
+        csv_writer.writerow([min_height])  # Line 3
 
     f.close()
     return misc_path
 
+
 # Helper Methods for Finding Maximums and Minimums of Each Attribute of <DataArray>
 def find_max_value(data_arr, attr):
-    tmp = float(data_arr[0][attr])
+    tmp_max = float(data_arr[0][attr])
     for i in range(len(data_arr)):
-        if float(data_arr[i][attr]) > tmp:
-            tmp = float(data_arr[i][attr])
-    return tmp
+        if float(data_arr[i][attr]) > tmp_max:
+            tmp_max = float(data_arr[i][attr])
+    return tmp_max
+
 
 def find_min_value(data_arr, attr):
-    tmp = float(data_arr[0][attr])
+    tmp_min = float(data_arr[0][attr])
     for i in range(len(data_arr)):
-        if float(data_arr[i][attr]) < tmp:
-            tmp = float(data_arr[i][attr])
-    return tmp
+        if float(data_arr[i][attr]) < tmp_min:
+            tmp_min = float(data_arr[i][attr])
+    return tmp_min
 
 
 # Latitude is DataArr[0], Longitude is DataArr[1], Height is DataArr[2], Slope is DataArr[3]
@@ -137,13 +143,10 @@ def find_min_lat(data_arr):
 
 x_and_y_dim = generate_data_array()
 
-absolute_min_height = find_min_height(dataArray)
-absolute_max_height = find_max_height(dataArray)
-abs_zero_height_scale = (abs(absolute_max_height) + abs(absolute_min_height))
+# absolute_min_height = find_min_height(dataArray)
+# absolute_max_height = find_max_height(dataArray)
+# abs_zero_height_scale = (abs(absolute_max_height) + abs(absolute_min_height))
 
 rect_file_path = write_rect_file(dataArray)
-misc_path = write_misc_file(absolute_min_height)
+# misc_path = write_misc_file(absolute_min_height)
 print("Data Processing Success")
-
-
-# TODO: Find a plausible landing zone, and find a way for the algorithm to get between it / i.e. we nee a startpoint and an endpoint for djikstra
