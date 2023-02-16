fbfa68c26b9f0d70a6276bf8546153c3f0c5f52d
Abhi Arya <89806154+abhi-arya1@users.noreply.github.com>
2023-02-16 14:15:57 -0800

    Update Artemis_ADC_Launcher.py

--- Artemis_ADC_Launcher.py
+++ Artemis_ADC_Launcher.py
@@ -1,13 +1,20 @@
-# This program is the central hub that the other programs are run from
-
-from subprocess import run
+# This program is the central hub that the other programs are run from #
 import os
+from subprocess import run
+
+code_path = os.getcwd()
+
+pathfinderpath = code_path + "/PathFinder.exe"
+pathfinder_program = run(["cmd", "/c", pathfinderpath])
+
+foldercreator_path = code_path + "/FolderCreator.py"
+foldercreator_program = run(["cmd", "/c", foldercreator_path])
 
 
 # runs DataProcessor.py
-data_processor_path = fc.path_sub2.replace("\\", "/") + "/DataProcessor.py"
+data_processor_path = code_path + "/DataProcessor.py"
 data_processor_program = run(["cmd", "/c", data_processor_path])
 
 # runs Cartographer.py
-cartographer_path = fc.path_sub2.replace("\\", "/") + "/Cartographer.py"
+cartographer_path = code_path + "/Cartographer.py"
 cartographer_program = run(["cmd", "/c", cartographer_path])
